10458780bf98d189ac4bb03ad1e20aff5909badd
jiihyun12 <j22h.h0h@gmail.com>
2025-07-19 21:56:00 +0900

    250719

---
+++ baekjoon/2025.07_week3/day05_num_24266.py
@@ -0,0 +1,16 @@
+# 250719
+
+# for i = 1 to n:
+#     for j = 1 to n:
+#         for k = 1 to n:
+#             sum++
+# 3중 for문  
+# 각 반복이 n번씩 
+# 총 수행 횟수는 n³
+
+# n³을 계산해서 출력
+
+n = int(input())
+
+print(n * n * n)
+print(3)
