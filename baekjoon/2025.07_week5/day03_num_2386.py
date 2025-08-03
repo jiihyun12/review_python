67a54d771abfa140059958a2e5cf9d6310c33140
jiihyun12 <j22h.h0h@gmail.com>
2025-08-03 20:39:57 +0900

    250803

---
+++ baekjoon/2025.07_week5/day03_num_2386.py
@@ -0,0 +1,14 @@
+# 250803
+
+while True:
+    n = input()
+
+    if n == '#' :
+        break
+
+    target = n[0].lower()
+    sentence = n[2:].lower()
+    count = sentence.count(target)
+
+    print(n[0], count)
+
