307c469e3e0520645f3019081e700e5422c93900
jiihyun12 <j22h.h0h@gmail.com>
2025-08-15 21:00:00 +0900

    250815

--- baekjoon/2025.08_week2/day01_num_2845.py
+++ baekjoon/2025.08_week2/day01_num_2845.py
@@ -1,8 +1,11 @@
 # 250813
 
+# 기자들이 5개의 기사에서 참석자 수를 썼는데, 실제 참석자수와 비교했을 때 얼마나 차이가 나는지?
+
 L, P = map(int, input().split())
 reuslt = L * P 
 nums = list(map(int, input().split()))
 
 for n in nums:
   print(n - reuslt, end = ' ')
+
