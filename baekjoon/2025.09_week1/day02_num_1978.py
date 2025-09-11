3a903cdc4aa8fb5a474e3d32d9a1a788e8e1a6e7
jiihyun12 <j22h.h0h@gmail.com>
2025-09-11 21:00:00 +0900

    250911

--- baekjoon/2025.09_week1/day02_num_1978.py
+++ baekjoon/2025.09_week1/day02_num_1978.py
@@ -1,1 +1,29 @@
 # 250911
+
+import math
+import sys
+
+input = sys.stdin.readline
+
+def is_prime(x: int) -> bool:
+    if x < 2:
+        return False
+    # 2는 소수
+    if x == 2:
+        return True
+    # 짝수이면 소수 아님
+    if x % 2 == 0:
+        return False
+    # 홀수만 검사: 3,5,7,...,sqrt(x)
+    limit = int(math.isqrt(x))
+    for d in range(3, limit + 1, 2):
+        if x % d == 0:
+            return False
+    return True
+
+_ = int(input().strip())
+nums = list(map(int, input().split()))
+
+cnt = sum(1 for v in nums if is_prime(v))
+print(cnt)
+
