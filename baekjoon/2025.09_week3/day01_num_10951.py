2f4cb15a13cd8ff81fd8497093a9038e0fb01d16
jiihyun12 <j22h.h0h@gmail.com>
2025-09-14 01:35:42 +0900

    250914_10951

--- baekjoon/2025.09_week3/day01_num_10951.py
+++ baekjoon/2025.09_week3/day01_num_10951.py
@@ -1,2 +1,13 @@
 # 250914
 
+while True:
+    try:
+        a,b = map(int, input().split())
+        print(a + b)
+    except:
+        break
+
+
+# ==================
+# try 안에서 입력을 계속 받음
+# 더 이상 입력이 없으면 EOFError가 발생 --> except에서 break
