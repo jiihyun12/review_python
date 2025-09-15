69972abca260982631d29cfb1c619c7c51a48915
jiihyun12 <j22h.h0h@gmail.com>
2025-09-15 21:35:29 +0900

    250915

---
+++ baekjoon/2025.09_week3/day03_num_15680.py
@@ -0,0 +1,21 @@
+# 250915
+
+
+N = int(input().strip())
+print("YONSEI" if N == 0 else "Leading the Way to the Future")
+
+
+# =================================
+
+# 정수 N이 주어짐 (0 또는 1).
+# N == 0이면 "YONSEI"
+# N == 1이면 "Leading the Way to the Future" 출력
+
+# 하나의 정수 N 입력 (0 또는 1)
+N = int(input().strip())
+
+# 조건에 따라 서로 다른 문자열 출력
+if N == 0:
+    print("YONSEI")
+else:  # N == 1
+    print("Leading the Way to the Future")
