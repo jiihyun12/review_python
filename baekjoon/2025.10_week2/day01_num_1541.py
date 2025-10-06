09d4843e9ac02b978a0433030dc00024a4739fc3
jiihyun12 <j22h.h0h@gmail.com>
2025-10-06 21:23:09 +0900

    1541-

--- baekjoon/2025.10_week2/day01_num_1541.py
+++ baekjoon/2025.10_week2/day01_num_1541.py
@@ -24,6 +24,16 @@ def sum_something(i):
 # 나머지 덩어리(sep[1:])는 전부 더한 뒤 전부 빼버림
 result = sum_something(sep[0]) - sum(sum_something(y) for y in sep[1:])
 
+#1️. sep[0] = '55'
+# sum_something('55') = 55
+# 첫 번째 조각은 그냥 더한다.
+# 2️. sep[1:] = ['50+40']
+# 각 조각마다 sum_something()을 적용해서 전부 더한다.
+# sum(sum_something(y) for y in sep[1:]) = 90
+# 뒤의 조각들은 모두 빼버린다.
+
+#수학적으로 괄호를 넣을 수 있는 방법 중 결과가 최소가 되게 하려면
+# 첫 - 이후에 나오는 건 전부 하나로 묶어서 빼는 게 제일 작게 나오기 때문에 이렇게 한다.
 print(result)
 
 
