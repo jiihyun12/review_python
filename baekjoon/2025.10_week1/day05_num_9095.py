756b4f9a48b3b1d61e6400bfcd21d131522480eb
jiihyun12 <j22h.h0h@gmail.com>
2025-10-08 22:33:20 +0900

    9095-

--- baekjoon/2025.10_week1/day05_num_9095.py
+++ baekjoon/2025.10_week1/day05_num_9095.py
@@ -1,4 +1,24 @@
 # 251004
 
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+dp = [0] * 11
+dp[1], dp[2], dp[3] = 1, 2, 4
+
+for i in range(4, 11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for _ in range(T):
+    n = int(input())
+    print(dp[n])
+
+
+
+
+# ======================================================
+
 # 정수 n이 주어졌을 때,
 # n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 문제
