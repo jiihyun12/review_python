0db4192a68ed0985449a74d7b1a9e5e8d350bb2a
jiihyun12 <j22h.h0h@gmail.com>
2025-10-08 22:19:55 +0900

    251008

--- baekjoon/2025.10_week2/day03_num_1003.py
+++ baekjoon/2025.10_week2/day03_num_1003.py
@@ -1,2 +1,23 @@
 # 251008
 
+test = int(input())
+
+count_zero = [0] * 41
+count_one = [0] * 41
+
+count_zero[0], count_one[0] = 1, 0
+count_zero[1], count_one[1] = 0, 1
+
+for i in range(2, 41):
+    count_zero[i] = count_zero[i - 1] + count_zero[i - 2]
+    count_one[i] = count_one[i - 1] + count_one[i - 2]
+
+for _ in range(test):
+    data = int(input())
+    print(count_zero[data], count_one[data])
+
+
+
+
+
+
