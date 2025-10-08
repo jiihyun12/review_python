9e59f06809c5c444743542a5be3e7f34a282d379
jiihyun12 <j22h.h0h@gmail.com>
2025-10-08 22:27:44 +0900

    11659

--- baekjoon/2025.10_week2/day02_num_11659.py
+++ baekjoon/2025.10_week2/day02_num_11659.py
@@ -1,2 +1,18 @@
 # 251007
 
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+summ = [0]
+
+for x in nums:
+    summ.append(summ[-1] + x)
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    print(summ[j] - summ[i - 1])
+
+
+
