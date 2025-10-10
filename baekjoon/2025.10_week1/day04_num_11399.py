ae4b67110a4b738b35352e8eae211aba9724e771
jiihyun12 <j22h.h0h@gmail.com>
2025-10-10 22:16:35 +0900

    11399

--- baekjoon/2025.10_week1/day04_num_11399.py
+++ baekjoon/2025.10_week1/day04_num_11399.py
@@ -1,2 +1,21 @@
 # 251003
 
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+times = list(map(int, input().split()))
+
+times.sort()
+
+total = 0      
+current = 0    
+
+for t in times:
+    current += t    
+    total += current  
+
+print(total)
+
+# ============================
