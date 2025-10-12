b655d9e85d6f7a0def0f6db4ee45746a20d2c136
jiihyun12 <j22h.h0h@gmail.com>
2025-10-12 20:11:49 +0900

    2438_

--- baekjoon/2025.10_week2/day06_num_2438.py
+++ baekjoon/2025.10_week2/day06_num_2438.py
@@ -2,13 +2,13 @@
 
 N = int(input())
 
-for i in range(1, N + 1):
-    print("*" * i)
+for i in range(1, N + 1):  # 1부터 N까지 반복 
+    print("*" * i)  # i번째 줄에서는 별을 i개 출력
 
 # =========================
 
 N = int(input())
 line = ""
 for _ in range(N):
-    line += "*"   
-    print(line)  
+    line += "*"   # 한 개씩 누적
+    print(line)   # 누적된 만큼 출력
