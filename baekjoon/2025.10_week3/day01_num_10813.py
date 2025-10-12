6d987f0f998dd1f2b03d51930c483390b503b7c8
jiihyun12 <j22h.h0h@gmail.com>
2025-10-12 20:17:09 +0900

    10813_

--- baekjoon/2025.10_week3/day01_num_10813.py
+++ baekjoon/2025.10_week3/day01_num_10813.py
@@ -1,2 +1,17 @@
 # 251013
 
+N, M = map(int, input().split())
+
+basket = []
+for i in range(N):
+    basket.append(i + 1)
+
+for _ in range(M):
+    i, j = map(int, input().split())
+
+    temp = basket[i - 1]
+    basket[i - 1] = basket[j - 1]
+    basket[j - 1] = temp
+
+for x in basket:
+    print(x, end=' ')
