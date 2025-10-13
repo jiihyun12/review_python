04208e2a916db12910d0d9576ab3060989a7b5f7
jiihyun12 <j22h.h0h@gmail.com>
2025-10-13 21:16:52 +0900

    1004_

--- baekjoon/2025.10_week3/day01_num_1004.py
+++ baekjoon/2025.10_week3/day01_num_1004.py
@@ -1,2 +1,24 @@
 # 251013
 
+import sys
+input = sys.stdin.readline
+
+import sys
+input = sys.stdin.readline
+
+def inside(px, py, cx, cy, r):
+    return (px - cx) * (px - cx) + (py - cy) * (py - cy) < r * r
+
+T = int(input())
+for _ in range(T):
+    x1, y1, x2, y2 = map(int, input().split())
+    
+    n = int(input())
+    default = 0
+    for _ in range(n):
+        cx, cy, r = map(int, input().split())
+        s_in = inside(x1, y1, cx, cy, r)
+        e_in = inside(x2, y2, cx, cy, r)
+        if s_in ^ e_in:
+            default += 1
+    print(default)
