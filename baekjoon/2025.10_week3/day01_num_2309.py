b39a7519f0ad7fc8d9778f21845c549865aadbf2
jiihyun12 <j22h.h0h@gmail.com>
2025-10-13 21:12:34 +0900

    251013

--- baekjoon/2025.10_week3/day01_num_2309.py
+++ baekjoon/2025.10_week3/day01_num_2309.py
@@ -6,3 +6,27 @@
 # 9명 중 2명만 가짜이고
 # 그 두 명을 찾아서 제외한 뒤
 # 남은 7명의 키를 오름차순으로 출력
+
+heights = [int(input().strip()) for _ in range(9)]
+
+total = sum(heights)
+need = total - 100
+
+seen = set()
+fake_a = None
+fake_b = None
+
+for h in heights:
+    complement = need - h
+    if complement in seen:
+        fake_a = h
+        fake_b = complement
+        break
+    seen.add(h)
+
+answer = heights[:]
+answer.remove(fake_a)
+answer.remove(fake_b)
+
+for x in sorted(answer):
+    print(x)
