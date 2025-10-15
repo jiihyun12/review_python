0c5ea895ba7a648a6b9914fc3adba4ad8bc2ace0
jiihyun12 <j22h.h0h@gmail.com>
2025-10-15 21:58:03 +0900

    11726

--- baekjoon/2025.10_week3/day02_num_11726.py
+++ baekjoon/2025.10_week3/day02_num_11726.py
@@ -1,2 +1,18 @@
 # 251015
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+MOD = 10007
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    dp1, dp2 = 1, 2   
+    for _ in range(3, n + 1):
+        dp1, dp2 = dp2, (dp1 + dp2) % MOD
+    print(dp2 % MOD)
