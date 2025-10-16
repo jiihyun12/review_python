000ffe1dc33267f37a5890e7f61c59011eca3128
jiihyun12 <j22h.h0h@gmail.com>
2025-10-16 15:23:45 +0900

    251016
    Added input handling and improved output formatting for the sequence generation.

--- baekjoon/2025.10_week3/day03_num_15650.py
+++ baekjoon/2025.10_week3/day03_num_15650.py
@@ -15,3 +15,34 @@ def result(start, depth):
         seq.pop()
 
 result(1, 0)
+
+
+# ======================================
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+seq = []
+
+def result(start, depth):
+    # depth가 M이면 지금까지 뽑은 seq를 문제 형식(공백으로)로 출력
+    if depth == M:
+        print(*seq)     # <-- 여기서 *를 써서 리스트를 풀어 공백으로 출력해야 함
+        return
+    # start부터 N까지 숫자를 하나씩 선택 (오름차순 유지)
+    for x in range(start, N + 1):
+        seq.append(x)
+        result(x + 1, depth + 1)
+        seq.pop()
+
+result(1, 0)
+
+# * --> 리스트를 풀어서(unpack) 출력하거나 함수 인자로 전달할 때 쓰는 문법
+# ex) 
+# nums = [1, 2, 3]
+# print(nums) --> [1,2,3]
+# print(*nums) --> 1,2,3
+
+
