db6dec251c9729dcb738aec146abf520dbfe4d4a
jiihyun12 <j22h.h0h@gmail.com>
2025-10-20 22:23:38 +0900

    16953

--- baekjoon/2025.10_week4/day1_num_16953.py
+++ baekjoon/2025.10_week4/day1_num_16953.py
@@ -1,1 +1,15 @@
 # 251020
+
+A, B = map(int, input().split())
+cnt = 1
+
+while B > A:
+    if B % 10 == 1:   
+        B //= 10
+    elif B % 2 == 0:  
+        B //= 2
+    else:            
+        break
+    cnt += 1
+
+print(cnt if A == B else -1)
