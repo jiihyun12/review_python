90fde3c8a1a8432b8d352df98299e51d1044cbe8
jiihyun12 <j22h.h0h@gmail.com>
2025-10-20 22:19:03 +0900

    251020

--- baekjoon/2025.10_week4/day01_num_2455.py
+++ baekjoon/2025.10_week4/day01_num_2455.py
@@ -1,2 +1,16 @@
 # 251020
 
+import sys
+input = sys.stdin.readline
+
+train = 0
+maxI = 0
+
+for _ in range(4):
+    a, b = map(int, input().split())
+    train = train - a + b
+    
+    if train > maxI:
+        maxI = train
+
+print(maxI)
