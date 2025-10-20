7bb03ffb7d6c95c5b0afa93f8c26281220495fd7
jiihyun12 <j22h.h0h@gmail.com>
2025-10-20 22:19:29 +0900

    2455

--- baekjoon/2025.10_week4/day01_num_2455.py
+++ baekjoon/2025.10_week4/day01_num_2455.py
@@ -14,3 +14,19 @@ for _ in range(4):
         maxI = train
 
 print(maxI)
+
+# =======================
+
+import sys
+input = sys.stdin.readline
+
+train = 0     # 현재 기차 안 인원
+maxI = 0      # 지금까지의 최대 인원
+
+for _ in range(4):
+    a, b = map(int, input().split())  # a: 내린 사람, b: 탄 사람
+    train = train - a + b             # 현재 인원 갱신
+    if train > maxI:                  # 최대 인원 갱신
+        maxI = train
+
+print(maxI)  # 마지막에 최대값 출력!
