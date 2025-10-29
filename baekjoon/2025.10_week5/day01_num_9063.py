85c09043050f056e7f29fc60b40973dabe24688e
jiihyun12 <j22h.h0h@gmail.com>
2025-10-29 22:42:07 +0900

    963

--- baekjoon/2025.10_week5/day01_num_9063.py
+++ baekjoon/2025.10_week5/day01_num_9063.py
@@ -1,2 +1,20 @@
 # 251029
 
+N = int(input())
+points = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+x_list = [p[0] for p in points]
+y_list = [p[1] for p in points]
+
+width = max(x_list) - min(x_list)
+height = max(y_list) - min(y_list)
+
+print(width * height)
+
+
+
+
+
