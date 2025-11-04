6bee3f821f7a4e1e48adf11a6c10a817e8636cda
jiihyun12 <j22h.h0h@gmail.com>
2025-11-05 08:19:18 +0900

    2577

--- baekjoon/2025.11_week1/day03_num_2577.py
+++ baekjoon/2025.11_week1/day03_num_2577.py
@@ -1,2 +1,11 @@
 # 251105
 
+A = int(input())
+B = int(input())
+C = int(input())
+
+result = A * B * C         
+result_str = str(result)    
+
+for i in range(10):         
+    print(result_str.count(str(i)))
