d8f7c16c4e76565a83fb1c1b2c1b0778e436da0c
jiihyun12 <j22h.h0h@gmail.com>
2025-11-14 17:04:47 +0900

    8958

--- baekjoon/2025.11_week2/day03_num_8958.py
+++ baekjoon/2025.11_week2/day03_num_8958.py
@@ -1,2 +1,19 @@
 # 251112
 
+T = int(input()) 
+
+for _ in range(T):
+    s = input().strip()  
+    score = 0             
+    count = 0             
+
+    for ch in s:
+        if ch == 'O':
+            count += 1       
+            score += count    
+        else:
+            count = 0        
+
+    print(score)
+
+
