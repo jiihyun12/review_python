897804834c9083fdd4852857506ab65e51590c02
jiihyun12 <j22h.h0h@gmail.com>
2025-11-20 12:51:44 +0900

    10250

--- baekjoon/2025.11_week3/day01_num_10250.py
+++ baekjoon/2025.11_week3/day01_num_10250.py
@@ -1,2 +1,15 @@
 # 251120
 
+T = int(input())
+
+for _ in range(T):
+    H, W, N = map(int, input().split())
+    
+    if N % H == 0:
+        floor = H
+        room = N // H
+    else:
+        floor = N % H
+        room = N // H + 1
+    
+    print(floor * 100 + room)
