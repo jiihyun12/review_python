7781b529beab47844b0fc70810c64671e661d5c8
jiihyun12 <j22h.h0h@gmail.com>
2025-11-27 14:56:52 +0900

    11650

--- baekjoon/2025.11_week4/day02_num_11650.py
+++ baekjoon/2025.11_week4/day02_num_11650.py
@@ -1,2 +1,18 @@
 # 251127
 
+import sys
+
+N = int(sys.stdin.readline().strip())
+points = []
+
+for _ in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    points.append((x, y))
+
+points.sort(key=lambda p: (p[0], p[1]))
+
+for x, y in points:
+    print(x, y)
+
+# ==============================
+
