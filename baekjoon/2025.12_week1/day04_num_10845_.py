5c8c82fffb125a803bbe597d3cd64802096bc159
jiihyun12 <j22h.h0h@gmail.com>
2025-12-10 19:57:14 +0900

    10845 V2

--- baekjoon/2025.12_week1/day04_num_10845_.py
+++ baekjoon/2025.12_week1/day04_num_10845_.py
@@ -1,2 +1,39 @@
 # 251210
 
+import sys
+from collections import deque
+
+input = sys.stdin.readline   
+N = int(input())             
+
+q = deque()                 
+
+for _ in range(N):
+    cmd = input().split()  
+
+    if cmd[0] == "push":
+        q.append(cmd[1])
+
+    elif cmd[0] == "pop":
+        if q:
+            print(q.popleft()) 
+        else:
+            print(-1)
+
+    elif cmd[0] == "size":
+        print(len(q))         
+
+    elif cmd[0] == "empty":
+        print(0 if q else 1)
+
+    elif cmd[0] == "front":
+        if q:
+            print(q[0])       
+        else:
+            print(-1)
+
+    elif cmd[0] == "back":
+        if q:
+            print(q[-1])    
+        else:
+            print(-1)
