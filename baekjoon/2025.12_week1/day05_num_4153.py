b5643882e2d4c6c9777f9b18cefb873136308a9a
jiihyun12 <j22h.h0h@gmail.com>
2025-12-12 09:05:22 +0900

    4153

--- baekjoon/2025.12_week1/day05_num_4153.py
+++ baekjoon/2025.12_week1/day05_num_4153.py
@@ -1,4 +1,13 @@
 # 251212
 
-
-
+while True:
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+    sides = [a, b, c]
+    sides.sort()       
+    x, y, h = sides[0], sides[1], sides[2]
+    if x*x + y*y == h*h:
+        print("right")
+    else:
+        print("wrong")
